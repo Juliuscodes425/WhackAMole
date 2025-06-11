74b75d51fb95f7bcb377b132617d70a6fbb70796
Dylan Jules <dylan.jules@oakwood.edu>
2025-06-11 00:23:04 -0400

    I do not know how to do this

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import WelcomeScreen from "./WelcomeScreen.jsx";
+
 export default function App() {
-  return <></>;
+  return (
+    <div>
+      <WelcomeScreen /> <div>Welcome to Whack-A-Mole!</div>
+    </div>
+  );
 }
---
+++ src/WelcomeScreen.jsx
@@ -0,0 +1,7 @@
+export default function WelcomeScreen() {
+  return (
+    <>
+      <h1>WhackThatMole</h1> <h2>This is the best game alive</h2>
+    </>
+  );
+}
